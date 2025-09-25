bd3447f16853180e45cb9bd7cc334ac01522cbc9
AlexGarcia99 <sauber.alex7@gmail.com>
2025-09-25 17:06:24 -0600

    Update Incidencias.py

--- Incidencias.py
+++ Incidencias.py
@@ -1,63 +1,64 @@
-#HGZ98
 from datetime import datetime, timedelta
 import re
-import pyodbc  # para SQL Server
-import win32com.client  # para Outlook automation
+import pyodbc
+import pandas as pd
+import win32com.client as win32
 
-# Parametros de conexion de la BD
-db_config = {
-    'server': 'Localhost',
-    'database': 'private',
-    'user': 'private',
-    'password': 'private',
-}
-
-# Configuracion de OutLook
-yesterday = datetime.now() - timedelta(days=1)
-outlook = win32com.client.Dispatch("Outlook.Application")
-namespace = outlook.GetNamespace("MAPI")
-recipient = "private@private.gob.mx"  # correo del jefe de servicio
-subject = f"Estudios realizados el {yesterday.strftime('%Y-%m-%d')} HGZ98"
-body = "Buenos días."
-
-def execute_sql_query(query):
-    connection = pyodbc.connect(
-        f"DRIVER={{SQL Server}};SERVER={db_config['server']};DATABASE={db_config['database']};"
-        f"UID={db_config['user']};PWD={db_config['password']}"
-    )
+# FUNCION PARA EJECUTAR QUERY Y DEVOLVER EL DATAFRAME DE PANDAS
+def execute_sql_query(connection_string, sql_query1):
+    connection = pyodbc.connect(connection_string)
     cursor = connection.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
+    result = pd.read_sql_query(sql_query1,connection)
     connection.close()
     return result
 
-def send_email(to_list, subject, body):
+# FUNCION PARA MANDAR EMAIL Y ADJUNTAR EXCEL
+def send_email_with_attachment(to_address, subject, body, attachment_path):
+    outlook = win32.Dispatch('Outlook.Application')
     mail = outlook.CreateItem(0)
-    mail.To = ";".join(to_list)
+    mail.To = to_address
     mail.Subject = subject
     mail.Body = body
+    mail.Attachments.Add(attachment_path)
     mail.Send()
 
-def main():
-    additional_recipient = "private@private.com.mx"  #email extra a copiar en el correo
-    # Query a la BD de MS SQL Server del total de estudios de un dia anteior(tomando en cuenta los VisiblePACS=1 y filtrando posibles UIDEstudio repetidos del dia). Apuntar a la BD(inmediatamente despues del FROM) en cuestion en el query.
-    sql_query = "SELECT COUNT(W.PARTICION_UID) AS ESTUDIOS FROM (SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY FOLIO ORDER BY (SELECT 1)) AS PARTICION_UID, * FROM [HIS_WEB].[dbo].[ImagenologiaEstudios] WHERE DATEADD(day, -1, convert(date, GETDATE())) = CONVERT(DATE, FECHAESTUDIO) AND VisiblePACS=1) AS T WHERE T.PARTICION_UID=1) AS W"
+# CREDENCIALES BD
+server = 'Localhost'
+database = 'private'
+username = 'private'
+password = 'private'
+connection_string = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
-    # Ejecuta el query
-    result = execute_sql_query(sql_query)
-    resultINT = list(map(int, re.findall('\d+', str(result))))
-    #resultSTRING = str(resultINT)
-    yesterday = datetime.now() - timedelta(days=1)
-    
-    # Procesa el resultado del query en el cuerpo del correo
-    email_body = f"Buenos días Dr. Flores.\n\nSe informa sobre la cantidad de estudios realizados.\n\n {yesterday.strftime('%Y-%m-%d')} : {resultINT} estudios. \n\nNota: Los estudios reportados pueden variar al final del mes debido a reconciliaciones solicitadas por el jefe de servicio o envío tardío de los mismos al servidor de imágenes.\n\nSaludos.\n\n*Correo automático" #concatenar el resultado del query(convertido a string) en esta linea
-    #for row in result:
-        #email_body += f"{row}\n"
+# QUERIES QUE EJECUTAN AMBOS SP GUARDADOS EN BD HISWEB PRODUCCION
+sql_query1 = 'EXEC ListadoPacientesMalLlenados'
+sql_query2 = 'EXEC TotalPacientesMalLlenados'
 
-    # Envia email
-    send_email([recipient, additional_recipient], subject, email_body)
+# EJECTUA QUERIES Y OBTIENE RESULTADO COMO UN DATAFRAME(SOLAMENTE EL sql_query1)
+result_df = execute_sql_query(connection_string, sql_query1)
+result2 = execute_sql_query(connection_string, sql_query2)
+resultList = list(map(int, re.findall('\d+', str(result2))))
+resultString = ' '.join(str (e) for e in resultList)
+#for x in str(resultList):
+#   resultString += ' ' + x
+#result_a_INT = int(resultINT)
+#resultFOR = [x [1] for x in result_a_INT]
 
-if __name__ == "__main__":
-    main()
+# GUARDA RESULTADO COMO ARCHIVO EXCEL
+excel_file_path = r"C:\Users\Administrador\Desktop\Pacientes mal llenados excel\HGZ98\PacientesMalLlenados_HGZ98.xlsx"
+result_df.to_excel(excel_file_path, index=False)
+
+# REMITENTE Y CUERPO DEL CORREO
+yesterday = datetime.now() - timedelta(days=1)
+week = datetime.now() - timedelta(days=7)
+to_email = 'miguel.floreshe@imss.gob.mx; jcolvera@cmr3.com.mx'
+email_subject = f"SMI DIG Incidencias presentadas entre fechas {week.strftime('%Y-%m-%d')} y {yesterday.strftime('%Y-%m-%d')}"
+email_body = f"Buenos días Dr. Flores.\n\nEn la semana dentro de las fechas {week.strftime('%Y-%m-%d')} y {yesterday.strftime('%Y-%m-%d')}, en referencia al Servicio Médico Integral de Digitalización, Post-Procesamiento, Almacenamiento y Distribución de la Imagen en la unidad médica HGZ 98 le comento que NO hubo incidencias relacionadas con el servicio.\n\nCantidad de estudios mal llenados: {resultString[2:]} \n\nMantenimientos Correctivos: 0\n\nSaludos.\n\n*Correo automático"
+
+# ENVIA CORREO CON EL EXCEL ADJUNTO
+send_email_with_attachment(to_email, email_subject, email_body, excel_file_path)
+
+print('CORREO ENVIADO EXITOSAMENTE.')
+
+
 
 
